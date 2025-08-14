7a29f5b311acbf743226df74856708aefe7e3c9c
theBappy <abappy575@gmail.com>
2025-08-14 12:13:38 +0600

    tanstack react query leveraging trpc for both speed and faster fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
-const Page = () => {
-  return <div>Page</div>;
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Client } from "./client";
+import { Suspense } from "react";
+
+const Page = async () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.hello.queryOptions({ text: "Hello John" })
+  );
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
+  );
 };
 
 export default Page;
